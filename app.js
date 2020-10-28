3fae207549579c8103884650cde3d9d203111478
Quinton Fults <quinton.fults.work@gmail.com>
2020-10-28 14:11:26 -0700

    code

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-document.getElementById('myHeading').textContent = 'Something Else'
+// DOM (Document Object Model) Manipulation
+
+// Grabbing an element from the HTML page
+// document.querySelector('#myHeading')
+// document.getElementById('myHeading')
+
+// You can change the text content or inner HTML of an element
+// document.getElementById('myHeading').textContent = 'Something Else'
+// document.getElementById('myHeading').innerHTML = '<button>Click Me</button>'
+

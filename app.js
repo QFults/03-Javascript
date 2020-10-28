0d6d2fb7e9da98511a42ccd0c76cdace19909840
Quinton Fults <quinton.fults.work@gmail.com>
2020-10-28 10:58:48 -0700

    code

---
+++ app.js
@@ -0,0 +1,36 @@
+
+// Function Definition
+
+// Hoisting allows this to be called before it is defined
+// myFunction()
+
+// function myFunction () {
+//   console.log('hi')
+// }
+
+// -------------------------
+
+// Function assigned to variable with arrow function
+// const myFunction = () => {
+//   console.log('hi')
+// }
+
+// myFunction()
+
+// -------------------------
+
+// Arrow function with multiple parameters
+// const myFunction = (message, num) => {
+//   console.log(`Message: ${message}`)
+//   console.log(`Number: ${num}`)
+// }
+
+// myFunction('Hello World!', 10)
+// myFunction('Goodbye World!')
+
+// -------------------------
+
+// Spooky Banana
+// ('b' + 'a' + + 'a' + 'a').toLowerCase()
+
+
